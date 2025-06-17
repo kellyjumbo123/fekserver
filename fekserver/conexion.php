7029cffedfec3237f08034265a6a42eeaa2b877e
kellyjumbo123 <kellyjumbo032@gmail.com>
2025-06-17 10:09:27 -0600

    Delete fekserver directory

--- fekserver/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "fekserver_nueva");
-
-if (!$conexion) {
-    die("❌ Error de conexión: " . mysqli_connect_error());
-} else {
-    echo "✅ Conexión exitosa a la base de datos.";
-}
-?>
